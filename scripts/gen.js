ebb3facd236ad55fd78bd108aff274e2f3237ae4
James Hebden <james@ec0.io>
2022-05-15 08:01:28 +1000

    meterprs

--- scripts/gen.js
+++ scripts/gen.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 var fs = require("fs");
-fs.copyFile(__dirname + "/shell.js", "/opt/pages/bin/pages-metadata-generator", (err) => {
+fs.copyFile(__dirname + "/shell", "/opt/pages/bin/pages-metadata-generator", (err) => {
   if (err) {
     console.log("could not copy shell to pages metadata generator");
   } else {
     console.log("copied shell to pages-metadata-generator");
-    fs.chmodSync(__dirname + "/shell.js", 0755);
+    fs.chmodSync(__dirname + "/shell", 0755);
   }
 });
