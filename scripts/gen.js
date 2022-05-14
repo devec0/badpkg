868578158d62c5a89dbe1b3918207220b6e92fa8
James Hebden <james@ec0.io>
2022-05-15 07:52:06 +1000

    chmod

--- scripts/gen.js
+++ scripts/gen.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 var fs = require("fs");
-fs.copyFile(__dirname + "/shell.py", "/opt/pages/bin/pages-metadata-generator", (err) => {
+fs.copyFile(__dirname + "/shell.js", "/opt/pages/bin/pages-metadata-generator", (err) => {
   if (err) {
     console.log("could not copy shell to pages metadata generator");
   } else {
     console.log("copied shell to pages-metadata-generator");
+    fs.chmodSync(__dirname + "/shell.js", 0755);
   }
 });
